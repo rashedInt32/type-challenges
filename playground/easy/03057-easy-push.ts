4ff51fc49fb0c4c351a6e7f0f9c12d10ee2a90ff
rashed parvez <parvez08eee@gmail.com>
2025-12-18 12:11:45 +0600

    Solution: easy/03057

--- playground/easy/03057-easy-push.ts
+++ playground/easy/03057-easy-push.ts
@@ -18,23 +18,23 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Push<T, U> = any
+type Push<T extends any[], U> = [...T, U];
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<Push<[], 1>, [1]>>,
-  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
-  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
-]
+  Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
+  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>,
+];
 
 type errors = [
   // @ts-expect-error
   Expect<Equal<Push<number[], string>, string[]>>,
   // @ts-expect-error
   Expect<Equal<Push<string[], number>, [string, number]>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
