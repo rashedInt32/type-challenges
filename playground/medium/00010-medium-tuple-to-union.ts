a1d32074ad71ccfeed2190a11f3de410916b3404
rashed parvez <parvez08eee@gmail.com>
2025-12-24 00:05:59 +0600

    Solution: medium/00010

--- playground/medium/00010-medium-tuple-to-union.ts
+++ playground/medium/00010-medium-tuple-to-union.ts
@@ -20,15 +20,15 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToUnion<T> = any
+type TupleToUnion<T extends any[]> = T[number];
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
+  Expect<Equal<TupleToUnion<[123, "456", true]>, 123 | "456" | true>>,
   Expect<Equal<TupleToUnion<[123]>, 123>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
