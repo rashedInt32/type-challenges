f2a21ee027ed2e0cfa70e09810800154fd44122b
rashed parvez <parvez08eee@gmail.com>
2025-12-26 16:39:06 +0600

    Solution: medium/00015

--- playground/medium/00015-medium-last-of-array.ts
+++ playground/medium/00015-medium-last-of-array.ts
@@ -24,17 +24,17 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Last<T extends any[]> = any
+type Last<T extends any[]> = T extends [...infer _, infer L] ? L : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<Last<[]>, never>>,
   Expect<Equal<Last<[2]>, 2>>,
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
   Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
