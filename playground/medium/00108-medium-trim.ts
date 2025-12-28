36196c43836c0fa48a8dec4a3fec240e871b397e
rashed parvez <parvez08eee@gmail.com>
2025-12-28 16:11:04 +0600

    Solution: medium/00108

--- playground/medium/00108-medium-trim.ts
+++ playground/medium/00108-medium-trim.ts
@@ -18,21 +18,26 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Trim<S extends string> = any
+type Space = " " | "\n" | "\t";
 
+type Trim<S extends string> = S extends
+  | `${Space}${infer T}`
+  | `${infer T}${Space}`
+  ? Trim<T>
+  : S;
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<Trim<'str'>, 'str'>>,
-  Expect<Equal<Trim<' str'>, 'str'>>,
-  Expect<Equal<Trim<'     str'>, 'str'>>,
-  Expect<Equal<Trim<'str   '>, 'str'>>,
-  Expect<Equal<Trim<'     str     '>, 'str'>>,
-  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
-  Expect<Equal<Trim<''>, ''>>,
-  Expect<Equal<Trim<' \n\t '>, ''>>,
-]
+  Expect<Equal<Trim<"str">, "str">>,
+  Expect<Equal<Trim<" str">, "str">>,
+  Expect<Equal<Trim<"     str">, "str">>,
+  Expect<Equal<Trim<"str   ">, "str">>,
+  Expect<Equal<Trim<"     str     ">, "str">>,
+  Expect<Equal<Trim<"   \n\t foo bar \t">, "foo bar">>,
+  Expect<Equal<Trim<"">, "">>,
+  Expect<Equal<Trim<" \n\t ">, "">>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
