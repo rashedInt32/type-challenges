576bdf6624f0eab668b162ed9bdafa881d657095
rashed parvez <parvez08eee@gmail.com>
2025-12-29 15:59:57 +0600

    Solution: medium/00191

--- playground/medium/00191-medium-append-argument.ts
+++ playground/medium/00191-medium-append-argument.ts
@@ -23,23 +23,25 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends (...args: any[]) => any, A> = (
+  ...args: [...Parameters<Fn>, A]
+) => ReturnType<Fn>;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
-type Case1 = AppendArgument<(a: number, b: string) => number, boolean>
-type Result1 = (a: number, b: string, x: boolean) => number
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>;
+type Result1 = (a: number, b: string, x: boolean) => number;
 
-type Case2 = AppendArgument<() => void, undefined>
-type Result2 = (x: undefined) => void
+type Case2 = AppendArgument<() => void, undefined>;
+type Result2 = (x: undefined) => void;
 
 type cases = [
   Expect<Equal<Case1, Result1>>,
   Expect<Equal<Case2, Result2>>,
   // @ts-expect-error
   AppendArgument<unknown, undefined>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
