75ee962ac1897dc9e6e4bcd67c6856b6dfc44e59
rashed parvez <parvez08eee@gmail.com>
2025-12-30 16:14:49 +0600

    Solution: medium/00296

--- playground/medium/00296-medium-permutation.ts
+++ playground/medium/00296-medium-permutation.ts
@@ -16,18 +16,42 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Permutation<T> = any
+type Permutation<T, U = T> = [T] extends [never]
+  ? []
+  : U extends U
+    ? [U, ...Permutation<Exclude<T, U>>]
+    : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<Permutation<'A'>, ['A']>>,
-  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
-  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<"A">, ["A"]>>,
+  Expect<
+    Equal<
+      Permutation<"A" | "B" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<"B" | "A" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
   Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
   Expect<Equal<Permutation<never>, []>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
