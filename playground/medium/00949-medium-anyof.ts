9af6b6b9c6ca6f65ed3b33d1e8eb9d4039978c21
rashed parvez <parvez08eee@gmail.com>
2026-01-18 16:32:42 +0600

    Solution: medium/00949

--- playground/medium/00949-medium-anyof.ts
+++ playground/medium/00949-medium-anyof.ts
@@ -19,23 +19,35 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AnyOf<T extends readonly any[]> = any
+type Falsy = 0 | "" | false | [] | Record<string, never> | null | undefined;
+
+type AnyOfRecur<T extends readonly any[]> = T extends [infer F, ...infer R]
+  ? F extends Falsy
+    ? AnyOf<R>
+    : true
+  : false;
+
+type AnyOf<T extends readonly any[]> = T[number] extends Falsy ? false : true;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
+  Expect<
+    Equal<AnyOf<[1, "test", true, [1], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[1, "", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "test", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { name: "test" }]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { 1: "test" }]>, true>>,
+  Expect<
+    Equal<AnyOf<[0, "", false, [], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[0, "", false, [], {}, undefined, null]>, false>>,
   Expect<Equal<AnyOf<[]>, false>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
