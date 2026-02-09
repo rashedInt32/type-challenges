97cfc5528acf622fb69dc49e44b52c0d0d0bebb0
rashed parvez <parvez08eee@gmail.com>
2026-02-09 17:46:01 +0600

    Solution: medium/01042

--- playground/medium/01042-medium-isnever.ts
+++ playground/medium/01042-medium-isnever.ts
@@ -23,20 +23,20 @@
 
 /* _____________ Your Code Here _____________ */
 
-type IsNever<T> = any
+type IsNever<T> = [T] extends [never] ? true : false;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<IsNever<never>, true>>,
   Expect<Equal<IsNever<never | string>, false>>,
-  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<"">, false>>,
   Expect<Equal<IsNever<undefined>, false>>,
   Expect<Equal<IsNever<null>, false>>,
   Expect<Equal<IsNever<[]>, false>>,
   Expect<Equal<IsNever<{}>, false>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
