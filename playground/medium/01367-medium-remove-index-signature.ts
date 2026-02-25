d0d56c8e99989377819b6ae461a040aba6675a2a
rashed parvez <parvez08eee@gmail.com>
2026-02-25 08:32:39 +0600

    Solution: medium/01367

--- playground/medium/01367-medium-remove-index-signature.ts
+++ playground/medium/01367-medium-remove-index-signature.ts
@@ -23,39 +23,47 @@
 
 /* _____________ Your Code Here _____________ */
 
-type RemoveIndexSignature<T> = any
+type RemoveIndexSignature<T> = {
+  [K in keyof T as string extends K
+    ? never
+    : number extends K
+      ? never
+      : symbol extends K
+        ? never
+        : K]: T[K];
+};
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type Foo = {
-  [key: string]: any
-  foo(): void
-}
+  [key: string]: any;
+  foo(): void;
+};
 
 type Bar = {
-  [key: number]: any
-  bar(): void
-  0: string
-}
+  [key: number]: any;
+  bar(): void;
+  0: string;
+};
 
-const foobar = Symbol('foobar')
+const foobar = Symbol("foobar");
 type FooBar = {
-  [key: symbol]: any
-  [foobar](): void
-}
+  [key: symbol]: any;
+  [foobar](): void;
+};
 
 type Baz = {
-  bar(): void
-  baz: string
-}
+  bar(): void;
+  baz: string;
+};
 
 type cases = [
   Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
-  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void, 0: string }>>,
+  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
   Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
-  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void, baz: string }>>,
-]
+  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
