af1f06c445fba8ba3f0e98a0a2128e157dcf95c6
rashed parvez <parvez08eee@gmail.com>
2026-03-06 01:03:15 +0600

    Solution: medium/02595

--- playground/medium/02595-medium-pickbytype.ts
+++ playground/medium/02595-medium-pickbytype.ts
@@ -23,23 +23,30 @@
 
 /* _____________ Your Code Here _____________ */
 
-type PickByType<T, U> = any
+type PickByType<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 interface Model {
-  name: string
-  count: number
-  isReadonly: boolean
-  isEnable: boolean
+  name: string;
+  count: number;
+  isReadonly: boolean;
+  isEnable: boolean;
 }
 
 type cases = [
-  Expect<Equal<PickByType<Model, boolean>, { isReadonly: boolean, isEnable: boolean }>>,
+  Expect<
+    Equal<
+      PickByType<Model, boolean>,
+      { isReadonly: boolean; isEnable: boolean }
+    >
+  >,
   Expect<Equal<PickByType<Model, string>, { name: string }>>,
   Expect<Equal<PickByType<Model, number>, { count: number }>>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
