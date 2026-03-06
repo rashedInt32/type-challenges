b95d1560efba2730bdcdf783d535797caeb3a928
rashed parvez <parvez08eee@gmail.com>
2026-03-06 23:49:03 +0600

    Solution: medium/02688

--- playground/medium/02688-medium-startswith.ts
+++ playground/medium/02688-medium-startswith.ts
@@ -20,20 +20,22 @@
 
 /* _____________ Your Code Here _____________ */
 
-type StartsWith<T extends string, U extends string> = any
+type StartsWith<T extends string, U extends string> = T extends `${U}${string}`
+  ? true
+  : false;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
-  Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
-  Expect<Equal<StartsWith<'abc', 'abc'>, true>>,
-  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
-  Expect<Equal<StartsWith<'abc', ''>, true>>,
-  Expect<Equal<StartsWith<'abc', ' '>, false>>,
-  Expect<Equal<StartsWith<'', ''>, true>>,
-]
+  Expect<Equal<StartsWith<"abc", "ac">, false>>,
+  Expect<Equal<StartsWith<"abc", "ab">, true>>,
+  Expect<Equal<StartsWith<"abc", "abc">, true>>,
+  Expect<Equal<StartsWith<"abc", "abcd">, false>>,
+  Expect<Equal<StartsWith<"abc", "">, true>>,
+  Expect<Equal<StartsWith<"abc", " ">, false>>,
+  Expect<Equal<StartsWith<"", "">, true>>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
