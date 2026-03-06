225dc3de995b26309580b36eba1fa45fc849407a
rashed parvez <parvez08eee@gmail.com>
2026-03-06 23:54:25 +0600

    Solution: meidum/02693

--- playground/medium/02693-medium-endswith.ts
+++ playground/medium/02693-medium-endswith.ts
@@ -20,19 +20,21 @@
 
 /* _____________ Your Code Here _____________ */
 
-type EndsWith<T extends string, U extends string> = any
+type EndsWith<T extends string, U extends string> = T extends `${string}${U}`
+  ? true
+  : false;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<EndsWith<'abc', 'bc'>, true>>,
-  Expect<Equal<EndsWith<'abc', 'abc'>, true>>,
-  Expect<Equal<EndsWith<'abc', 'd'>, false>>,
-  Expect<Equal<EndsWith<'abc', 'ac'>, false>>,
-  Expect<Equal<EndsWith<'abc', ''>, true>>,
-  Expect<Equal<EndsWith<'abc', ' '>, false>>,
-]
+  Expect<Equal<EndsWith<"abc", "bc">, true>>,
+  Expect<Equal<EndsWith<"abc", "abc">, true>>,
+  Expect<Equal<EndsWith<"abc", "d">, false>>,
+  Expect<Equal<EndsWith<"abc", "ac">, false>>,
+  Expect<Equal<EndsWith<"abc", "">, true>>,
+  Expect<Equal<EndsWith<"abc", " ">, false>>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
