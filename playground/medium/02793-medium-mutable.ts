7a14ff84af6362e231f4a50dcf611677553e6b17
rashed parvez <parvez08eee@gmail.com>
2026-03-09 23:57:10 +0600

    Solution: medium/02793

--- playground/medium/02793-medium-mutable.ts
+++ playground/medium/02793-medium-mutable.ts
@@ -25,33 +25,35 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Mutable<T> = any
+type Mutable<T> = {
+  -readonly [P in keyof T]: T[P];
+};
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 interface Todo1 {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
   meta: {
-    author: string
-  }
+    author: string;
+  };
 }
 
-type List = [1, 2, 3]
+type List = [1, 2, 3];
 
 type cases = [
   Expect<Equal<Mutable<Readonly<Todo1>>, Todo1>>,
   Expect<Equal<Mutable<Readonly<List>>, List>>,
-]
+];
 
 type errors = [
   // @ts-expect-error
-  Mutable<'string'>,
+  Mutable<"string">,
   // @ts-expect-error
   Mutable<0>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
