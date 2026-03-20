cd03ef29454978ed9356572eaeb323241acee052
rashed parvez <parvez08eee@gmail.com>
2026-03-20 11:51:12 +0600

    Solution: medium/03196

--- playground/medium/03196-medium-flip-arguments.ts
+++ playground/medium/03196-medium-flip-arguments.ts
@@ -21,27 +21,42 @@
 
 /* _____________ Your Code Here _____________ */
 
-type FlipArguments<T> = any
+type Reverse<T extends unknown[]> = T extends [infer F, ...infer R]
+  ? [...Reverse<R>, F]
+  : [];
+
+type FlipArguments<T extends (...args: any[]) => any> = T extends (
+  ...args: infer P
+) => infer U
+  ? (...args: Reverse<P>) => U
+  : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<FlipArguments<() => boolean>, () => boolean>>,
-  Expect<Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>>,
-  Expect<Equal<FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>, (arg0: boolean, arg1: number, arg2: string) => void>>,
-]
+  Expect<
+    Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>
+  >,
+  Expect<
+    Equal<
+      FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>,
+      (arg0: boolean, arg1: number, arg2: string) => void
+    >
+  >,
+];
 
 type errors = [
   // @ts-expect-error
-  FlipArguments<'string'>,
+  FlipArguments<"string">,
   // @ts-expect-error
-  FlipArguments<{ key: 'value' }>,
+  FlipArguments<{ key: "value" }>,
   // @ts-expect-error
-  FlipArguments<['apple', 'banana', 100, { a: 1 }]>,
+  FlipArguments<["apple", "banana", 100, { a: 1 }]>,
   // @ts-expect-error
   FlipArguments<null | undefined>,
-]
+];
 
 /* _____________ Further Steps _____________ */
 /*
